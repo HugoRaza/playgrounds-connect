940b155ec88aa993fd303ca230893d67412a85bc
hugo razafindranaly <hugo.raza@gmail.com>
2019-04-10 12:52:22 +0200

    parsing the playground name and playground city

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'open-uri'
+require 'nokogiri'
+
+city = 'lyon'
+url = "https://www.courtsoftheworld.com/France/#{city}"
+
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+all_name = []
+all_address = []
+
+html_doc.css("div.text h5 a").each do |e|
+  playground_name = e.text.strip
+  all_name << playground_name
+end
+
+html_doc.css("div.text .courtdetail").each do |e|
+  playground_address = e.text.strip.split(' ').join(' ')
+  all_address << playground_address
+end
+
+p1_name = all_name[0]
+p1_address = all_address[0]
+puts "#{p1_name} - #{p1_address}"
+puts all_name.length
+puts all_address.length
+
+# html_doc.search('.text').each do |element|
+#   title = element.text.strip
+#   puts title
+#   puts "---------"
+# end
