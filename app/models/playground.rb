c640c5558206cda3386baaff58a96f28810381a1
Aymeric Vinot <aymeric.vinot@gmail.com>
2019-04-16 12:33:14 +0200

    seed finished

--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,6 +1,6 @@
 class Playground < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # after_validation :geocode, if: :will_save_change_to_address?
 
   has_many :participations
   validates :name, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,13 @@ URL_ROOT = "https://www.courtsoftheworld.com/France/"
 TOP_CITIES.each do |city|
   begin
     html_doc = Nokogiri::HTML(open(URL_ROOT + city).read)
+    court_details = html_doc.css("div.text .courtdetail")
 
-    html_doc.css("div.text h5 a").each do |e|
-      Playground.create!(city: city, name: e.text.strip, address: e.text.strip.split(' ').join(' '))
+    html_doc.css("div.text h5 a").each_with_index do |e, index|
+      Playground.create!(city: city, name: e.text.strip, address: court_details[index].text.strip.split(' ').join(' '))
     end
 
+    sleep(0.5)
     p "playgrounds created in #{city}"
   rescue StandardError => e
     puts e.message
