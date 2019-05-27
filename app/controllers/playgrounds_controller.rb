90f690424e9c6a5e05abafeec3922dddf212e092
hugo razafindranaly <hugo.raza@gmail.com>
2019-05-27 17:49:13 +0200

    updating status and add rake into the controller participation

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -1,6 +1,7 @@
 class PlaygroundsController < ApplicationController
   def index
     @playgrounds = Playground.search(params[:city])
+    async_rake('update_stuff:participations', options = {})
     if @playgrounds.empty?
       redirect_to new_playground_path, notice: "Be the first to add a Playground in #{params[:city][0]} "
     end
@@ -39,5 +40,21 @@ class PlaygroundsController < ApplicationController
     params.require(:playground).permit(:name, :address, :rims_number, :playgound_picture, :term)
   end
 
+  def async_rake(task, options = {})
+    options[:rails_env] ||= Rails.env
+
+    # format the options as unix environmental variables
+    env_vars = options.map { |key, value| "#{key.to_s.upcase}='#{value.to_s}'" }
+    env_vars_string = env_vars.join(' ')
+
+    log_file = File.join(Rails.root, "log/async_rake.#{Rails.env}.log")
+
+    # fire and forget
+    Process.fork {
+      exec("#{env_vars_string} bin/rake #{task} --trace 2>&1 >> #{log_file}")
+    }
+    # or:
+    # system("#{env_vars_string} bin/rake #{task} --trace 2>&1 >> #{log_file} &")
+  end
 
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,12 +3,12 @@ class Participation < ApplicationRecord
   belongs_to :playground
 
   def update_status
-    if start_date > Time.now
-      status = "coming"
-    elsif end_date < Time.now
-      status = "finished"
-    else
+    if start_date < Time.now && end_date > Time.now
       status = "ongoing"
+    elsif start_date < Time.now && end_date < Time.now
+      status = "coming"
+    elsif start_date > Time.now && end_date > Time.now
+      status = "finished"
     end
     self.update!(status: status)
   end
